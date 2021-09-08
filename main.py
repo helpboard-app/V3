4fb791a1df9f7aeed98f6861933b38c727da5d01
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2021-09-07 19:24:01 -0500

    Now have routes

--- main.py
+++ main.py
@@ -3,11 +3,26 @@
 # Powered By Deta.sh
 # Built by @Brenden2008
 
-from fastapi import FastAPI
+# Import Libraries
+from fastapi import FastAPI, Form
+from deta import Deta
 
+# Start Common Tools
+deta = Deta(${{ secrets.DETA_PROJECT_TOKEN }})
 app = FastAPI()
 
+# Start Databases
+users = deta.Base("users")
+boards = deta.Base("boards")
+questions = deta.Base("questions")
 
+# Start Routes
+# Default Route | Displays the message below
 @app.get("/")
 def read_root():
     return "Helpboard API Version 3.0.0 | Powered by Deta.sh!"
+
+# User Create Route | Creates a user
+@app.post("user/signup/")
+async def login(username: str = Form("username"), password: str = Form("password")):
+    return {"username": username}
