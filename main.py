5f7bf0a7d092755c43d7303ae385ab96c44e16b1
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2021-09-13 17:44:49 -0500

    WOO

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Built by @Brenden2008
 
 # Import Libraries
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Response
 from deta import Deta
 import os
 from pydantic import BaseModel
@@ -17,6 +17,8 @@ app = FastAPI()
 users = deta.Base("users")
 boards = deta.Base("boards")
 questions = deta.Base("questions")
+sessions = deta.Base("sessions")
+userdata = deta.Base("userdata")
 
 # Declare Pydantic Models
 class User(BaseModel):
@@ -33,10 +35,32 @@ def Home_Page():
 
 # User Create Route | Creates a user
 @app.post("/user/create")
-async def create_user(user: User):
-    try:
-        users.put({"username": user.username, "password": user.password, "email": user.email}, user.username)
-    except:
-        return {success: 0}
+async def create_user(user: User, response: Response):
+    if users.get(user.username) != user.username:
+        session_key = random.randrange(0000000, 9999999, 3)
+        try:
+            users.put({"username": user.username, "password": user.password, "email": user.email}, user.username)
+            sessions.put({"session_key": session_key}, user.username)
+        except:
+            return "Failed to make user"
+        else:
+            response.set_cookie(key="session_key", value=session_key)
+            response.set_cookie(key="username", value=user.username)
+            return "Success"
     else:
-        return {success: 1}
+        return "User exists"
+
+
+# User Read Route | Get user data
+@app.post("/user/login")
+async def login_user(user: User, response: Response):
+    session_key = random.randrange(0000000, 9999999, 3)
+    try:
+        if users.get(user.username).password == user.password and users.get(user.username).username == user.username:
+            sessions.put({"session_key": session_key}, user.username)
+    except:
+        return "Failed to login user"
+    else:
+        response.set_cookie(key="session_key", value=session_key)
+        response.set_cookie(key="username", value=user.username)
+        return "Success"
