7765c30c86cfff06533fb9c8f39ecc0783ff22f4
Brenden2008 <brendenstahle@gmail.com>
2021-10-19 13:59:25 +0000

    Ok :fingerscrossed:

--- API-Dev/index.js
+++ API-Dev/index.js
@@ -11,40 +11,17 @@ const authkeys = deta.Base("authkeys");
 const helpboards = deta.Base("helpboards");
 const questions = deta.Base("questions");
 
-
 // Functions
-function getCryptoRandomBetween(min, max){
-  //the highest random value that crypto.getRandomValues could store in a Uint32Array
-  var MAX_VAL = 4294967295;
-  
-  //find the number of randoms we'll need to generate in order to give every number between min and max a fair chance
-  var numberOfRandomsNeeded = Math.ceil((max - min) / MAX_VAL);
-  
-  //grab those randoms
-  var cryptoRandomNumbers = new Uint32Array(numberOfRandomsNeeded);
-  crypto.getRandomValues(cryptoRandomNumbers);
-  
-  //add them together
-  for(var i = 0, sum = 0; i < cryptoRandomNumbers.length; i++){
-    sum += cryptoRandomNumbers[i];
-  }
-  
-  //and divide their sum by the max possible value to get a decimal
-  var randomDecimal = sum / (MAX_VAL * numberOfRandomsNeeded);
-  
-  //if result is 1, retry. otherwise, return decimal.
-  return randomDecimal === 1 ? getCryptoRandomBetween(min, max) : Math.floor(randomDecimal * (max - min + 1) + min);
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
 }
-
-function getRandomChar(str){
-  return str.charAt(getCryptoRandomBetween(0, str.length - 1));
-}
-
-String.random = function(length, characters) {
-  for(var i = 0, str = ""; i < length; i++) str += getRandomChar(characters);
-  return str;
-};
-
 
 const app = express(); 
 
@@ -63,7 +40,7 @@ app.post('/user/create', function(req, res){
     try {
       users.put({username: req.body.username, password: req.body.password}, req.body.username)
       try {
-        var authkey = String.random(256, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        var authkey = makeid(256)
         authkeys.put({username: req.body.username, authkey: authkey}, authkey)
         res.cookie('auth', authkey).send({success: 1, statuscode: 101})
       } catch (error) {
