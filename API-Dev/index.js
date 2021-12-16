3485e1a468fe70a9dc14b202fc3cada7bf7570da
Brenden2008 <brendenstahle@gmail.com>
2021-12-16 20:54:20 +0000

    lessgooo

--- API-Dev/index.js
+++ API-Dev/index.js
@@ -3,6 +3,16 @@ const { Deta } = require('deta');
 var multer = require('multer');
 var upload = multer();
 var cookieParser = require('cookie-parser');
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.auth0key,
+  baseURL: 'https://hbv3-dev.deta.dev',
+  clientID: 'pExK4AD4X4wNZFtNWthbVpD0budOK2Kf',
+  issuerBaseURL: 'https://boardapps.us.auth0.com'
+};
 
 const deta = Deta();
 // Declaring all the databases we need
@@ -18,9 +28,14 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); 
 app.use(upload.array()); 
 app.use(express.static('public'));
+app.use(auth(config));
 
 app.get("/", async (req, res) => {
   res.send("Helpboard API Server | Version 3 | Powered by Deta.sh, Made by Brenden2008")
+});
+
+app.get('/login/authcheck', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 module.exports = app;
