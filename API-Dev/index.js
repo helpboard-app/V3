962d19f83810b2e5e2eac239ef6d4e1f19fa9afa
Brenden2008 <brendenstahle@gmail.com>
2022-01-10 18:48:08 +0000

    lol

--- API-Dev/index.js
+++ API-Dev/index.js
@@ -11,13 +11,13 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.auth0key,
-  baseURL: 'https://xrh4b6.deta.dev/',
+  baseURL: 'https://hbv3-dev.deta.dev/',
   clientID: 'pExK4AD4X4wNZFtNWthbVpD0budOK2Kf',
   issuerBaseURL: 'https://boardapps.us.auth0.com'
 };
 
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const deta = Deta();
@@ -25,12 +25,12 @@ const deta = Deta();
 const helpboards = deta.Base("helpboards");
 const questions = deta.Base("questions");
 
-const app = express(); 
+const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true })); 
-app.use(upload.array()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.array());
 app.use(express.static('public'));
 app.use(auth(config));
 app.use('/static', express.static('static'))
@@ -49,11 +49,11 @@ app.get('/profile', requiresAuth(), (req, res) => {
 app.get('/helpboard/create', requiresAuth(), (req, res) => {
   var helpboard_id = getRndInteger(111111111, 999999999).toString();
   try {
-    helpboards.insert({helpboard_id: helpboard_id, helpboard_owner: req.oidc.user.email, helpboard_active: true, key: helpboard_id});
+    helpboards.insert({ helpboard_id: helpboard_id, helpboard_owner: req.oidc.user.email, helpboard_active: true, key: helpboard_id });
   } catch {
-    res.send({success: 0, err: 'Helpboard creation failed. Please try again.'})
+    res.send({ success: 0, err: 'Helpboard creation failed. Please try again.' })
   } finally {
-    res.send(JSON.stringify({success: 1, helpboard_id: helpboard_id, helpboard_owner: req.oidc.user.email, helpboard_active: true}));
+    res.send(JSON.stringify({ success: 1, helpboard_id: helpboard_id, helpboard_owner: req.oidc.user.email, helpboard_active: true }));
   }
 });
 
@@ -66,30 +66,30 @@ app.post('/helpboard/delete', requiresAuth(), (req, res) => {
     helpboard.then((data) => {
       if (data.helpboard_owner == email) {
         helpboards.delete(helpboard_id);
-        res.send({success: 1})
+        res.send({ success: 1 })
       } else {
-        res.send({success: 0, err: 'Helpboard deletion failed. Not your helpboard.'})
+        res.send({ success: 0, err: 'Helpboard deletion failed. Not your helpboard.' })
       };
     });
   } catch {
-    res.send({success: 0, err: 'Helpboard deletion failed. Please try again.'})
+    res.send({ success: 0, err: 'Helpboard deletion failed. Please try again.' })
   };
 });
 
 // Add a question
 app.post('/question/add', requiresAuth(), (req, res) => {
-    try {
-      var helpboard_id = req.body.helpboard_id;
-      var question = req.body.question;
-      var nickname = req.oidc.user.nickname;
-      var email = req.oidc.user.email;
-      var idgen = crypto.randomBytes(20).toString('hex');
-      var id = idgen;
-      var dbquestion = questions.put({nickname: nickname, question: question, email: email, helpboard: helpboard_id, question_id: id}, id);
-      res.send({success: 1, question_id: id})
-    } catch {
-      res.send("{success: 0}")
-    }
+  try {
+    var helpboard_id = req.body.helpboard_id;
+    var question = req.body.question;
+    var nickname = req.oidc.user.nickname;
+    var email = req.oidc.user.email;
+    var idgen = crypto.randomBytes(20).toString('hex');
+    var id = idgen;
+    var dbquestion = questions.put({ nickname: nickname, question: question, email: email, helpboard: helpboard_id, question_id: id }, id);
+    res.send({ success: 1, question_id: id })
+  } catch {
+    res.send("{success: 0}")
+  }
 });
 
 // Get a question
@@ -102,8 +102,8 @@ app.post('/question/get', requiresAuth(), (req, res) => {
     var helpboard = helpboards.get(helpboard_id);
     dbquestion.then((data) => {
       helpboard.then((data1) => {
-        if(email == data1.helpboard_owner){
-          res.send({success: 1, nickname: data.nickname, question: data.question, email: data.email, helpboard: data.helpboard, question_id: data.question_id})
+        if (email == data1.helpboard_owner) {
+          res.send({ success: 1, nickname: data.nickname, question: data.question, email: data.email, helpboard: data.helpboard, question_id: data.question_id })
         } else {
           res.send("{success: 0, err: 'Not your helpboard.'}")
         }
@@ -124,7 +124,7 @@ app.post('/question/delete', requiresAuth(), (req, res) => {
     var helpboard = helpboards.get(helpboard_id);
     dbquestion.then((data) => {
       helpboard.then((data1) => {
-        if(email == data1.helpboard_owner){
+        if (email == data1.helpboard_owner) {
           questions.delete(id);
           res.send("{success: 1}")
         } else {
@@ -138,27 +138,25 @@ app.post('/question/delete', requiresAuth(), (req, res) => {
 });
 
 // List questions
-app.post('/question/all', requiresAuth(), (req, res) => {
+app.post('/question/all', requiresAuth(), async (req, res) => {
   try {
     var helpboard_id = req.body.helpboard_id;
     var email = req.oidc.user.email;
-    var id = req.body.question_id;
-    var dbquestion = questions.fetch({"helpboard": helpboard_id});
-    var helpboard = helpboards.get(helpboard_id);
-    dbquestion.then((data) => {
-      helpboard.then((data1) => {
-        if(email == data1.helpboard_owner){
-          for (let index = 0; index < data.length; ++index) {
-            const element = theArray[index];
-            console.log(element.nickname);
-          }
-        } else {
-          res.send("{success: 0, err: 'Not your helpboard.'}")
-        }
-      })
-    });
-  } catch {
-    res.send("{success: 0}")
+    var dbquestion = await questions.fetch({ "helpboard": helpboard_id });
+    var helpboard = await helpboards.get(helpboard_id);
+    if (email == helpboard.helpboard_owner) {
+      var question_list = [];
+      for (let index = 0; index < dbquestion.count; ++index) {
+        var element = dbquestion.items[index];
+        question_list.push(element);
+      }
+      res.send(question_list);
+    } else {
+      res.send("{success: 0, err: 'Not your helpboard.'}");
+    };
+  } catch (err) {
+    res.send("{success: 0}");
+    console.log(err);
   }
 });
 
